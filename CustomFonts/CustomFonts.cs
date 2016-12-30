fdf4d7c4f5a8046d6652a233e964406c872e70c1
Kimserey Lam <kimserey.lam@gmail.com>
2016-12-30 21:19:18 +0000

    added font awesome

---
+++ CustomFonts/CustomFonts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CustomFonts
+{
+	public class FontAwesome
+	{
+		public static char FileO = '\xf016';
+		public static char SmileO = '\xf118';
+		public static char Taxi = '\xf1ba';
+		public static char Whatsapp = '\xf232';
+	}
+
+	public class IconLabel : Label
+	{
+		public IconLabel()
+		{
+			Margin = 2;
+		}
+	}
+
+	public class App : Application
+	{
+		public App()
+		{
+			var content = new ContentPage
+			{
+				Title = "Custom Fonts",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Orientation = StackOrientation.Horizontal,
+					Children = {
+						new IconLabel { Text = FontAwesome.FileO.ToString() },
+						new IconLabel { Text = FontAwesome.SmileO.ToString() },
+						new IconLabel { Text = FontAwesome.Taxi.ToString() },
+						new IconLabel { Text = FontAwesome.Whatsapp.ToString() }
+					}
+				}
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+	}
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics;
+
+namespace CustomFonts.Droid
+{
+	public class IconRenderer : LabelRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+			var label = (TextView)Control;
+			var font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, "fontawesome-webfont.ttf");
+			label.Typeface = font;
+		}
+	}
+
+	[Activity(Label = "CustomFonts.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+		}
+	}
+}
