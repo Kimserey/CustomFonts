945c04b701a63f73a9802294046d72eeca6cc56a
Kimserey Lam <kimserey.lam@gmail.com>
2017-01-01 15:26:09 +0000

    wrap layout

--- CustomFonts/CustomFonts.cs
+++ CustomFonts/CustomFonts.cs
@@ -28,16 +28,31 @@ namespace CustomFonts
 				Title = "Custom Fonts sample",
 				Content = new StackLayout
 				{
-					Margin = 10,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
 					Children = {
 						new Label { Text = "Hello world" },
 						new WrapLayout {
 							Children = {
-								new IconLabel { Text = FontAwesome.FileO.ToString() },
-								new IconLabel { Text = FontAwesome.SmileO.ToString() },
-								new IconLabel { Text = FontAwesome.Taxi.ToString() },
-								new IconLabel { Text = FontAwesome.Whatsapp.ToString() }
+								new IconLabel { Text = FontAwesome.FileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.SmileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Taxi.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Whatsapp.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.FileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.SmileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Taxi.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Whatsapp.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.FileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.SmileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Taxi.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Whatsapp.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.FileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.SmileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Taxi.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Whatsapp.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.FileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.SmileO.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Taxi.ToString(), WidthRequest = 30, HeightRequest = 30 },
+								new IconLabel { Text = FontAwesome.Whatsapp.ToString(), WidthRequest = 30, HeightRequest = 30 }
 							}
 						}
 					}
--- CustomFonts/WrapLayout.cs
+++ CustomFonts/WrapLayout.cs
@@ -6,52 +6,81 @@ namespace Xamarin.Forms
 {
 	public class WrapLayout : Layout<View>
 	{
-		double _w = 100;
-		double _h = 100;
-
 		protected override void LayoutChildren(double x, double y, double width, double height)
 		{
 			var regions = ComputeLayout(width, height);
 
-			foreach (var region in regions)
+			foreach (var region in regions.Item1)
 			{
-				LayoutChildIntoBoundingRegion(region.View, new Rectangle(x + X * _w, y + Y * _h, _w, _h)); 
+				LayoutChildIntoBoundingRegion(
+					region.View,
+					new Rectangle(
+						region.X * region.SizeRequest.Request.Width, 
+						region.Y * region.SizeRequest.Request.Height, 
+						region.SizeRequest.Request.Width, 
+						region.SizeRequest.Request.Height));
 			}
 		}
 
-		//protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
-		//{
-		//	double lastX;
-		//	double lastY;
-		//	var layout = ComputeLayout(widthConstraint, heightConstraint, out lastX, out lastY);
-
-		//	return new SizeRequest(new Size(lastX, lastY));
-		//}
-
-		List<Region> ComputeLayout(double width, double height)
+		protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
 		{
+			var regions = ComputeLayout(widthConstraint, heightConstraint);
+			var w = regions.Item2;
+			var h = regions.Item3;
+			return new SizeRequest(new Size(w, h));
+		}
+
+		Tuple<List<Region>, double, double> ComputeLayout(double widthConstraint, double heightConstraint)
+		{
+			var children = Children.FirstOrDefault();
+
+			if (children == null)
+				return new Tuple<List<Region>, double, double>(new List<Region>(), 0, 0);
+
+			var size = children.Measure(double.PositiveInfinity, double.PositiveInfinity);
+
 			var list = new List<Region>();
 
 			int x = 0;
 			int y = 0;
+			double rowWidth = 0;
+			double rowHeight = 0;
+			double width = 0;
+			double height = size.Request.Height;
 
 			foreach (var child in Children)
 			{
-				list.Add(new Region { 
+				if (size.Request.Width + rowWidth > widthConstraint)
+				{
+					if (width < rowWidth)
+						width = rowWidth;
+
+					x = 0;
+					y += 1;
+				    rowWidth = 0;
+					height += rowHeight;
+					rowHeight = size.Request.Height;
+				}
+
+				var region = new Region
+				{
 					View = child,
 					X = x,
-					Y = y
-				});
+					Y = y,
+					SizeRequest = size
+				};
+
+				list.Add(region);
 
 				x += 1;
+				rowWidth += size.Request.Width;
+				width += size.Request.Width;
 
-				if (x * _w > width)
-				{
-					y += 1;
-				}
+				if (rowHeight < size.Request.Height)
+					rowHeight = size.Request.Height;
 			}
 
-			return list;
+			return new Tuple<List<Region>, double, double>(list, width, height);
 		}
 
 		public class Region
@@ -59,6 +88,7 @@ namespace Xamarin.Forms
 			public View View { get; set; }
 			public double X { get; set; }
 			public double Y { get; set; }
+			public SizeRequest SizeRequest { get; set; }
 		}
 	}
 }
